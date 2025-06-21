e7a09745b7a642d29e56e2c6c8755cdfc129617a
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2025-06-21 12:09:33 -0400

    Update route.ts - change from testing to production

--- app/api/members/pending/route.ts
+++ app/api/members/pending/route.ts
@@ -5,17 +5,19 @@ import PendingMember from "@/lib/models/PendingMember";
 import logger from "@/lib/logger";
 
 export async function GET(req: Request) {
-  // try {
-  //   await requireRole(req as any, ["superadmin", "admin"]);
-  // } catch (err: any) {
-  //   logger.warn({ err }, "Unauthorized pending list attempt");
-  //   return NextResponse.json(
-  //     { error: err.message },
-  //     { status: err.statusCode }
-  //   );
-  // } // To test this on postman comment this try catch block and use the below code.
+  // PRODUCTION:
+  try {
+    await requireRole(req as any, ["superadmin", "admin"]);
+  } catch (err: any) {
+    logger.warn({ err }, "Unauthorized pending list attempt");
+    return NextResponse.json(
+      { error: err.message },
+      { status: err.statusCode }
+    );
+  } // To test this on postman comment this try catch block and use the below code.
 
-  const admin = { clerkId: "local-test", role: "superadmin" }; // This is just for testing the API. Uncomment the above try catch block in PRODUCTION and delete this line.
+  // TESTING: This is just for testing the API. Uncomment the above try catch block in PRODUCTION and delete this line.
+  // const admin = { clerkId: "local-test", role: "superadmin" };
 
   await connectDB();
   const list = await PendingMember.find({ status: "pending" }).lean();
